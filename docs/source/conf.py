226687909e14d98fa58e52d658f62861de69277c
bbauer02 <bbauer02@gmail.com>
2022-02-22 00:46:35 +0100

    Section 1 : Docker

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cours BTS SIO'
+copyright = '2021, Bauer Baptiste'
+author = 'Bauer Baptiste'
 
 release = '0.1'
 version = '0.1.0'
